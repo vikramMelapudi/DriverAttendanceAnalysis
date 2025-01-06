228341a2d55e824cb379a312c504f31d1c9b6cb9
Bhushan Patil <Bhushan.Patil@Intangles.ai>
2025-01-06 16:37:10 +0530

    updated

--- code/Attendance_validation.py
+++ code/Attendance_validation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import difflib
+import csv
 
 
 #https://stackoverflow.com/questions/41192424/python-how-to-correct-misspelled-names
@@ -26,53 +27,62 @@ IN_LIST = ['10-Dec','11-Dec','12-Dec','13-Dec','14-Dec','15-Dec','16-Dec','17-De
 #print(OSRTC_df[['Name of SO', 'Total']])
 #print(INTAG_df[['Driver Name', 'Total Working Days']])
 
+with open('Result.csv', 'w', newline='') as file:
 
-for os_drv_cnt in range(0,len(OSRTC_df['Name of SO'])):
-	#print(OSRTC_df['Name of SO'][os_drv_cnt])
-	NAME = OSRTC_df['Name of SO'][os_drv_cnt]
-	print('---------------------------------')
+	writer = csv.writer(file)
+	field = ["OSRTC Name", "Intangle Name", "Accuracy", "Match Days", "Miss Match Days"]
+	writer.writerow(field)
 
-	FLAG = 0 
+	for os_drv_cnt in range(0,len(OSRTC_df['Name of SO'])):
+		#print(OSRTC_df['Name of SO'][os_drv_cnt])
+		NAME = OSRTC_df['Name of SO'][os_drv_cnt]
+		print('---------------------------------')
 
-	for in_drv_cnt in range(0, len(INTAG_df['Driver Name'])):
-		OSRTC_S = NAME
-		INTAG_S = INTAG_df['Driver Name'][in_drv_cnt]
-		R = is_similar(OSRTC_S.lower(),INTAG_S.lower(), 0.9)
-		DAY_ACC = 0
-		if(R):
+		FLAG = 0 
+
+		for in_drv_cnt in range(0, len(INTAG_df['Driver Name'])):
+			OSRTC_S = NAME
+			INTAG_S = INTAG_df['Driver Name'][in_drv_cnt]
+			R = is_similar(OSRTC_S.lower(),INTAG_S.lower(), 0.9)
+			DAY_ACC = 0
+			if(R):
+				print('OSRTC ENTRY----->',OSRTC_S)
+				#print(OSRTC_df['Total'][os_drv_cnt])
+				print('INTANGLE ENTRY----->', INTAG_S)
+				#print(INTAG_df['Total Working Days'][in_drv_cnt])
+				
+				#print(INTAG_df['01-Dec'][in_drv_cnt])
+				for day_cnt in range(0,len(OS_LIST)):
+					OS_A = OSRTC_df[OS_LIST[day_cnt]][in_drv_cnt]
+					#print(OS_A)
+					IN_A = INTAG_df[IN_LIST[day_cnt]][in_drv_cnt]
+					#print(float(IN_A[0:2]))
+					if((OS_A == 1) and (float(IN_A[0:2]) > 0.5)):
+						DAY_ACC = DAY_ACC + 1
+						#print('MATCH')
+					if((OS_A == 'WO') and (float(IN_A[0:2]) == 0)):
+						DAY_ACC = DAY_ACC + 1
+						#print('MATCH1')
+					if((OS_A == 'A') and (float(IN_A[0:2]) == 0)):
+						DAY_ACC = DAY_ACC + 1
+						#print('MATCH2')
+					if((OS_A == 'L') and (float(IN_A[0:2]) == 0)):
+						DAY_ACC = DAY_ACC + 1
+						#print('MATCH3')
+				#exit(0)
+				FLAG = 1
+				print('ACCURACY------------------->', (DAY_ACC/len(OS_LIST))*100)
+				print('Number Of match days------->', DAY_ACC)
+				print('Number Of miss-match days-->', (len(OS_LIST) - DAY_ACC))
+				ROW = [OSRTC_S, INTAG_S , (DAY_ACC/len(OS_LIST))*100 , DAY_ACC , (len(OS_LIST) - DAY_ACC)]
+				writer.writerow(ROW)
+
+
+		if(FLAG < 1):
 			print('OSRTC ENTRY----->',OSRTC_S)
-			#print(OSRTC_df['Total'][os_drv_cnt])
-			print('INTANGLE ENTRY----->', INTAG_S)
-			#print(INTAG_df['Total Working Days'][in_drv_cnt])
-			
-			#print(INTAG_df['01-Dec'][in_drv_cnt])
-			for day_cnt in range(0,len(OS_LIST)):
-				OS_A = OSRTC_df[OS_LIST[day_cnt]][in_drv_cnt]
-				#print(OS_A)
-				IN_A = INTAG_df[IN_LIST[day_cnt]][in_drv_cnt]
-				#print(float(IN_A[0:2]))
-				if((OS_A == 1) and (float(IN_A[0:2]) > 0.5)):
-					DAY_ACC = DAY_ACC + 1
-					#print('MATCH')
-				if((OS_A == 'WO') and (float(IN_A[0:2]) == 0)):
-					DAY_ACC = DAY_ACC + 1
-					#print('MATCH1')
-				if((OS_A == 'A') and (float(IN_A[0:2]) == 0)):
-					DAY_ACC = DAY_ACC + 1
-					#print('MATCH2')
-				if((OS_A == 'L') and (float(IN_A[0:2]) == 0)):
-					DAY_ACC = DAY_ACC + 1
-					#print('MATCH3')
-			#exit(0)
-			FLAG = 1
-			print('ACCURACY------------------->', (DAY_ACC/len(OS_LIST))*100)
-			print('Number Of match days------->', DAY_ACC)
-			print('Number Of miss-match days-->', (len(OS_LIST) - DAY_ACC))
-
-
-	if(FLAG < 1):
-		print('OSRTC ENTRY----->',OSRTC_S)
-		print('NO INTANGLE ENTRY FOUND FOR DRIVER')
+			print('NO INTANGLE ENTRY FOUND FOR DRIVER')
+			ROW = [OSRTC_S, 'NA' , 'NA' , 'NA' , 'NA']
+			writer.writerow(ROW)
 	
 
 
